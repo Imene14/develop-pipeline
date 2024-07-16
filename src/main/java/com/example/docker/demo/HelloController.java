33222e692d10c56e925d946fb74667336063c5f2
Imene <imen.zarga1@gmail.com>
2024-07-16 12:31:06 +0200

    first commit

--- src/main/java/com/example/docker/demo/HelloController.java
+++ src/main/java/com/example/docker/demo/HelloController.java
@@ -1,16 +1,15 @@
 package com.example.docker.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.Map;
 
-@Controller
-public class AppController {
-    @GetMapping("/")
-    public String hello() {
-        return "index";
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Map<String,String> Sayhello() {
+        return Collections.singletonMap("message", "Hello world");
     }
 }

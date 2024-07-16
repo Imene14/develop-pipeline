d5975e89edb8758b9aacf95e8b4d8bd31cf68461
Imene <imen.zarga1@gmail.com>
2024-07-16 10:59:39 +0200

    first commit

--- src/main/java/com/example/docker/demo/AppController.java
+++ src/main/java/com/example/docker/demo/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping
     public String hello() {
-        return "HIII!!! my name is Imen, HII !!! my age is 21, nice to meet you";
+        return "HIII!!! my name is Imen, HII !!! my age is 21,";
     }
 }

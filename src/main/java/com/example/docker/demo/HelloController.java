168aa2f6deae7d830ced1b4129d31ce6334d69ca
Imene <imen.zarga1@gmail.com>
2024-07-19 11:45:04 +0200

    first commit

--- src/main/java/com/example/docker/demo/HelloController.java
+++ src/main/java/com/example/docker/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.docker.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;

6895f33da19bd079ea60be0a91cbd41a48284573
Imen14 <imen.zarga1@gmail.com>
2024-08-04 23:15:41 +0100

    modified from mobile

--- src/main/java/com/example/docker/demo/HelloController.java
+++ src/main/java/com/example/docker/demo/HelloController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class HelloController {
     @GetMapping("/hello")
     public Map<String,String> Sayhello() {
-        return Collections.singletonMap("message", "Hello world");
+        return Collections.singletonMap("message", "Hello world!!!");
     }
     @PostMapping("/submit")
     public Map<String,String> submit() {

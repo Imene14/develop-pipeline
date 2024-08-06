3aa46a1d518dc8b4d96947523582461f2f1468ff
Imene <imen.zarga1@gmail.com>
2024-08-06 23:22:53 +0200

    first commit

--- src/main/java/com/example/docker/demo/HelloController.java
+++ src/main/java/com/example/docker/demo/HelloController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class HelloController {
     @GetMapping("/hello")
     public Map<String,String> Sayhello() {
-        return Collections.singletonMap("message", "Hello world!!! SECOND CHANGE FOR TODAYYY !");
+        return Collections.singletonMap("message", "Hello world!!! SECOND CHANGE FOR TODAYYY !my name is imen");
     }
     @PostMapping("/submit")
     public Map<String,String> submit() {

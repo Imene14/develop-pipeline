5cc49eb880d774418550b0581b41aab4a5fac849
Imene <imen.zarga1@gmail.com>
2024-08-19 12:52:49 +0200

    first commit

--- src/main/java/com/example/docker/demo/user/UserController.java
+++ src/main/java/com/example/docker/demo/user/UserController.java
@@ -2,19 +2,32 @@ package com.example.docker.demo.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/")
 public class UserController {
     @Autowired
     private UserRepo userRepository;
-    @GetMapping("/users")
+    @GetMapping("/api/v1/users")
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }

105b3cba5555e5a35b22833933b84d0cc6378caa
Imene <imen.zarga1@gmail.com>
2024-08-22 00:10:26 +0200

    first commit

--- src/main/java/com/example/docker/demo/AppController.java
+++ src/main/java/com/example/docker/demo/AppController.java
@@ -1,7 +1,12 @@
 package com.example.docker.demo;
 
+import com.example.docker.demo.user.User;
+import com.example.docker.demo.user.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -27,6 +32,21 @@ public class AppController {
     @GetMapping("/user/{id}")
     public String DisplayUser() {
         return "user";
+    }
+
+    @Autowired
+    private UserRepo userRepository;
+    @PostMapping("api/v1/login")
+    public String loginUser(@RequestParam("username") String username,
+                            @RequestParam("password") String password) {
+        User user = userRepository.findByName(username);
+        // Implement your login logic here
+        if (user != null)  {
+            // Replace with actual authentication logic
+            return "redirect:/index";  // Redirect to a home page after successful login
+        } else {
+            return "redirect:/login?error";  // Redirect back to login with an error
+        }
     }
 
 }
